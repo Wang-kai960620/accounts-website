e168d41ebced8781f899ab1740a1ec687193108b
wangkai <wk.dong.up@gmail.com>
2020-07-29 10:52:21 +0800

    deploy

---
+++ precache-manifest.d5741319db5417e7f6a3a4d08a162f4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814a5c1db18d6b863cfd5d25fa4daed6",
+    "url": "/accounts-website/QRcode.png"
+  },
+  {
+    "revision": "fcbecc30fb8f7ba4dd6d",
+    "url": "/accounts-website/css/app.040a7a02.css"
+  },
+  {
+    "revision": "4bcfe04ced8937194affa6f95302d74b",
+    "url": "/accounts-website/index.html"
+  },
+  {
+    "revision": "fcbecc30fb8f7ba4dd6d",
+    "url": "/accounts-website/js/app.7ba314cc.js"
+  },
+  {
+    "revision": "2cecc624144038b073d8",
+    "url": "/accounts-website/js/chunk-vendors.55a5f1a7.js"
+  },
+  {
+    "revision": "26ed1defac65085b9bc82a018d2aca2b",
+    "url": "/accounts-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounts-website/robots.txt"
+  }
+]);
